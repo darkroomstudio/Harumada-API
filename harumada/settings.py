695ee466a6f52a61874ce8231f35725a4b2aaa9c
jeonghoyang12 <wjdwjdgh6998@gmail.com>
2024-11-25 21:21:00 -0500

    include CORS setting

--- harumada/settings.py
+++ harumada/settings.py
@@ -55,16 +55,18 @@ INSTALLED_APPS = [
     'users',
     'goals',
     'goal_sharing',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',    
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',    
 ]
 
 ROOT_URLCONF = 'harumada.urls'
